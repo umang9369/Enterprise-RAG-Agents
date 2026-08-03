378de35a5f2a5b3eaeec2c1763972a51df11cbe6
umang9369 <singhranju149@gmail.com>
2026-08-04 02:10:33 +0530

    opening of bs4 file initialisation.

--- app/ingestion/loaders/html.py
+++ app/ingestion/loaders/html.py
@@ -6,3 +6,14 @@ def parse_html(file_path:str):
     Parses HTML content using BeautifulSoup.
     Cleans scripts, styles, and extracts readable text for RAG.
     """
+    with logfire.span("html parsing",filename=file_path):
+        try:
+            with open(file_path,"r",encoding="utf-8",errors="ignore") as f:
+                content=f.read()
+
+            soup = BeautifulSoup(content,"html.parser")
+
+            
+
+
+        except Exception as e:
