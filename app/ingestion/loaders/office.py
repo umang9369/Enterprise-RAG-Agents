4c840142be60342f1043f3734bca4c67b45eb029
umang9369 <singhranju149@gmail.com>
2026-08-04 02:49:33 +0530

    .

--- app/ingestion/loaders/office.py
+++ app/ingestion/loaders/office.py
@@ -12,12 +12,7 @@ def parse_office(file_path: str):
             elements=partition(filename=file_path)
             full_text="\n".join([str(el) for el in elements])
 
-            if not full_text.strip():
-                logfire.warning(f"office parsing failed for : {file_path}")
-            else:
-                logfire.info(f"succesfully parsed office file {len(full_text)} characters")
-
-            return full_text
+            
         except Exception as e:
             logfire.error(f"office parsing failed for : {e}")
             raise e
