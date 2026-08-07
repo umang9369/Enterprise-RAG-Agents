c3ca9ea054ce9b432f189a7477b754b4d19bffa7
umang9369 <singhranju149@gmail.com>
2026-08-08 04:29:09 +0530

    fucntion making of tex parser for ingestion of text data

--- app/ingestion/loaders/text.py
+++ app/ingestion/loaders/text.py
@@ -6,4 +6,9 @@ def parse_text(file_path:str):
     """
 
     with logfire.span("parse_text",filename=file_path):
-        
+        try:
+            with open(file_path,"r",encoding="utf-8",errors="ignore")as f:
+                return f.read()
+        except Exception as e:
+            logfire.error(f"Error occurred while parsing text file: {e}")
+            raise e
