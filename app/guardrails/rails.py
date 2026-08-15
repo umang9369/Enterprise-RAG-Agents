d19f0825d83cd60cefeaee0ec9f6d33397f9f968
umang9369 <singhranju149@gmail.com>
2026-08-16 03:27:57 +0530

    importing modules and files

---
+++ app/guardrails/rails.py
@@ -0,0 +1,8 @@
+import logfire
+from langchain_openai import ChatOpenAI
+from nemoguardrails import LLMRails, RailsConfig
+
+from app.config import settings
+from app.guardrails.colang_rules import COLANG_CONTENT, RAIL_INDICATORS, YAML_CONTENT
+
+_rails: LLMRails | None = None
